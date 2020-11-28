a17eb60cc37dbb099edd86ea672e19b59235e59f
ruvalter <ruvalter@gmail.com>
2020-11-28 11:12:57 -0500

    Fix logos

--- src/containers/links/Links.js
+++ src/containers/links/Links.js
@@ -3,15 +3,22 @@ import LinkList from '../../components/link-list/LinkList';
 import './Links.scss';
 import LinksService from '../../shared/links-service';
 
+const addThemeClass = (theme) => document.body.classList.add(theme);
+
+const removeThemeClass = (theme) => document.body.classList.remove(theme);
+
 const Links = () => {
   document.title = 'Links | Link Explorer';
   const [links, updateLinks] = useState([]);
 
   useEffect(() => {
+    const theme = 'theme-portugues-ludico';
+    addThemeClass(theme);
     const fetchData = async () => {
       updateLinks(await new LinksService().getAllLinks());
     };
     fetchData();
+    return () => removeThemeClass(theme);
   }, []);
 
   return (
